cc3f712773d10c5bda81c5ffc3a5675b2d9e8c76
MediRa <medirahmat83@gmail.com>
2025-06-22 21:32:15 +0700

    finish

--- index.js
+++ index.js
@@ -1,86 +1,98 @@
-const express = require('express');
-const dotenv = require('dotenv');
+const express = require("express");
+const dotenv = require("dotenv");
 
-const multer = require('multer');
+const multer = require("multer");
 
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const { GoogleGenerativeAI} = require('@google/generative-ai');
-const imageToGenerativePart = require('@google/generative-ai');
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const { buffer } = require("stream/consumers");
+// const imageToGenerativePart = require("@google/generative-ai");
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 
 // Get the ai Model
-const GenAi = new GoogleGenerativeAI(process.env.GEMINI_API_KEY_2_0_FLASH)
+const GenAi = new GoogleGenerativeAI(process.env.GEMINI_API_KEY_2_0_FLASH);
 const model = GenAi.getGenerativeModel({
-    model:'models/gemini-2.0-flash'
+  model: "models/gemini-2.0-flash",
 });
 
-const upload = multer({dest: 'uploads/'});
+const upload = multer({ dest: "uploads/" });
 const PORT = 3000;
 
-app.listen(PORT, ()=> {
-    console.log(`Gemini api server running at http://localhost:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Gemini api server running at http://localhost:${PORT}`);
 });
 
 //add endpoint to generate text
-app.post('/generate-text', async (req, res) => {
-    const {prompt} = req.body;
-    try {
-        const result = await model.generateContent(prompt);
-        const response = result.response;
+app.post("/generate-text", async (req, res) => {
+  const { prompt } = req.body;
+  try {
+    const result = await model.generateContent(prompt);
+    const response = result.response;
 
-        res.json({output: response.text()})
-    } catch (error) {
-        res.status(500).json(
-            {error: error.message}
-        );
-    }
+    res.json({ output: response.text() });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
-//add endpoint to generate image
-app.post('/generate-from-image', upload.single('image'), async(req, res) => {
-    const {prompt} = req.body.prompt || 'Describe the image';
-    const image = imageToGenerativePart(req.file.path);
-
-    try {
-        const result = await model.generateContent([prompt, image]);
-        const response = await result.response;
-        res.json({output: response.text()});
-    } catch (error) {
-        res.status(500).json({error: error.message});
-    } finally {
-        fs.unlinkSync(req.file.path);
-    }
-});
-
-
-// // add endpoint to generate document
-app.post('/generate-audio'), upload.single('document'), async (req, res) => {
-    const filepath = req.file.path;
-    const buffer = fs.readFileSync(filepath);
-    const base64Data = buffer.toString('base64');
-    const mimeType = req.file.mimeType;
-
-    try {
-        const documentPart = {
-            inlineData : { data: base64Data, mimeType}
-        }
-
-        const result = await model.generateContent([
-            "Analyze the document",
-            documentPart
-        ]);
-
-        const response = await result.response;
-        res.json({output: response.text()});
-    } catch (error) {
-        res.status(500).json({error: error.message});
-    } finally {
-        fs.unlinkSync(filepath);
-    }
+function imageToGenerativePart(imagePath, mimeType = "image/jpeg") {
+  return {
+    inlineData: {
+      data: Buffer.from(fs.readFileSync(imagePath)).toString("base64"),
+      mimeType,
+    },
+  };
 }
 
+//add endpoint to generate image
+app.post("/generate-from-image", upload.single("image"), async (req, res) => {
+  const prompt = req.body.prompt || "Describe the image";
+  const image = imageToGenerativePart(req.file.path);
+
+  try {
+    const result = await model.generateContent([prompt, image]);
+    const response = result.response;
+
+    res.json({
+      output: response.text(),
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  } finally {
+    fs.unlinkSync(req.file.path);
+  }
+});
+
+// // add endpoint to generate document
+app.post(
+  "/generate-from-document",
+  upload.single("document"),
+  async (req, res) => {
+    const filepath = req.file.path;
+    const buffer = fs.readFileSync(filepath);
+    const base64Data = buffer.toString("base64");
+    const mimeType = req.file.mimetype;
+
+    try {
+      const documentPart = {
+        inlineData: { data: base64Data, mimeType },
+      };
+      const result = await model.generateContent([
+        "Analyze this document:",
+        documentPart,
+      ]);
+      const response = result.response;
+
+      res.json({ output: response.text() });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    } finally {
+      fs.unlinkSync(filepath);
+    }
+  },
+);
